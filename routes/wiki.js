542b391b4e733756fbbb51af4ca19a75eae3fa39
gg447062 <gg447062@gmail.com>
2021-03-11 16:11:40 -0600

    finishes #4

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,41 +1,55 @@
-
-
-
-const express = require('express')
+const express = require("express");
 let router = express.Router();
-const {addPage} = require('../views')
-const {Page} = require('../models')
-
-
+const { addPage, wikiPage, main } = require("../views");
+const { Page } = require("../models");
 
 //GET /wiki/
-router.get('/', (req, res, next) => {
-  res.send()
-})
+router.get("/", async (req, res, next) => {
+  try {
+    let pages = await Page.findAll();
+    console.log("PAGES OBJ", pages);
+    res.send(main(pages));
+  } catch (error) {
+    next(error);
+  }
+});
 
 //post /wiki/
-router.post('/', async (req, res, next) => {
-
+router.post("/", async (req, res, next) => {
   // res.send('POST WIKI')
   const title = req.body.title;
-  
- 
-  
-  
+
   try {
     const page = await Page.create({
       title: title,
       content: req.body.content,
     });
-    
+
     // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
     res.redirect(`/wiki/${page.slug}`);
-  } catch (error) { next(error) }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
 //GET /wiki/add/
-router.get('/add', (req, res, next) => {
-  res.send(addPage())
-})
+router.get("/add", (req, res, next) => {
+  res.send(addPage());
+});
 
-module.exports = router
+// GET /wiki/:slug
+router.get("/:slug", async (req, res, next) => {
+  try {
+    const slug = req.params.slug;
+    const page = await Page.findAll({
+      where: {
+        slug: slug,
+      },
+    });
+    res.send(wikiPage(page[0]));
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- views/main.js
+++ views/main.js
@@ -1,16 +1,33 @@
 const html = require("html-template-tag");
 const layout = require("./layout");
 
-module.exports = (pages) => layout(html`
-  <h3>Pages</h3>
-  <hr>
-  <form method="GET" action="/wiki/search">
-    <input type="text" name="search" />
-    <button type="submit">Search</button>
-  </form>
-  <hr>
-  <ul class="list-unstyled">
-    <ul>
-      <!-- PLACEHOLDER LIST OF PAGES -->
-    </ul>
-  </ul>`);
+module.exports = (pages) =>
+  layout(html` <h3>Pages</h3>
+    <hr />
+    <form method="GET" action="/wiki/search">
+      <input type="text" name="search" />
+      <button type="submit">Search</button>
+    </form>
+    <hr />
+    <ul class="list-unstyled">
+      <ul>
+        ${pages.map(
+          (page) => html`<li>
+            <a href="/wiki/${page.dataValues.slug}">${page.dataValues.title}</a>
+          </li>`
+        )}
+      </ul>
+    </ul>`);
+
+// ${posts.map(post => html`
+//     <div class='news-item'>
+//       <p>
+//         <span class="news-position">${post.id}. ▲</span>
+//         <a href="/posts/${post.id}">${post.title}</a>
+//         <small>(by ${post.name})</small>
+//       </p>
+//       <small class="news-info">
+//         ${post.upvotes} upvotes | ${timeAgo(post.date)}
+//       </small>
+//     </div>`
+//   )}
